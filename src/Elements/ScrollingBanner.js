15a22ee9b4c5fba3a6ba77ac920509405bbb92ad
Glizzy <gliccy.g@outlook.com>
2025-09-10 23:16:44 +0930

    Scrolling banner yet to be functional.

--- src/Elements/ScrollingBanner.js
+++ src/Elements/ScrollingBanner.js
@@ -2,30 +2,36 @@ class ScrollingBanner extends HTMLElement{
     constructor(){
         super();
 
-        this.left;
-        this.right;
+        this.display;
         this.text;
+
+        this.displayPosition;
     }
 
     connectedCallback(){
         this.style.display = "flex";
 
+        this.text = "ASDF";
+
+        this.displayPosition = 0;
+
         const shadow = this.attachShadow({mode:"open"});
 
-        this.left = document.createElement("span");
-        this.right = document.createElement("span");
+        this.display = document.createElement("span");
 
-        this.right.style.marginLeft = "auto";
+        this.display.style.position = "absolute";
 
-        this.left.innerText = "AS";
-        this.right.innerText = "DF";
+        this.display.innerText = this.text;
 
-        shadow.appendChild(this.left);
-        shadow.appendChild(this.right);
+        shadow.appendChild(this.display);
 
         setInterval(() => {
-            
-        }, interval);
+            this.displayPosition -= 1;
+            this.display.style.left = toString(this.displayPosition)+"px";
+            if(this.display.clientLeft<(0-this.display.clientWidth)){
+                this.display.clientLeft = this.clientWidth-this.display.clientWidth;
+            }
+        }, 1);
     }
 }
 
