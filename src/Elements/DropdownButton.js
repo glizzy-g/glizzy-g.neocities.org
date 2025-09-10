6ef50709e732a55bb731c815d4e225f3e493eba0
Glizzy <gliccy.g@outlook.com>
2025-09-10 12:33:36 +0930

    Added title. Added static news bar. Added dropdown custom element and added to page. Added empty settings page linked by index.

---
+++ src/Elements/DropdownButton.js
@@ -0,0 +1,45 @@
+class DropdownButton extends HTMLElement{
+    constructor(){
+        super();
+
+        this.button;
+        this.dropdown;
+        this.dropdownEnabled = false;
+    }
+
+    connectedCallback(){
+        const shadow = this.attachShadow({mode:"open"});
+
+        this.button = document.createElement("button");
+        this.dropdown = document.createElement("div");
+
+        this.button.part = "button";
+        this.dropdown.part = "dropdown";
+
+        this.button.textContent = this.textContent;
+
+        this.dropdown.style.cssText = "position:absolute;border-radius:3px;border:solid 2px black;visibility:hidden;padding:4px;";
+
+        this.button.addEventListener("click", (e)=>{
+            this.dropdownEnabled = !this.dropdownEnabled;
+            if(this.dropdownEnabled){
+                this.dropdown.style.visibility = "visible";
+            }else{
+                this.dropdown.style.visibility = "hidden";
+            }
+        });
+        this.addEventListener("mouseenter", function(e){
+            this.dropdown.style.visibility = "visible";
+        });
+        this.addEventListener("mouseleave", function(e){
+            if(!this.dropdownEnabled){
+                this.dropdown.style.visibility = "hidden";
+            }
+        });
+
+        shadow.appendChild(this.button);
+        shadow.appendChild(this.dropdown);
+    }
+}
+
+export default DropdownButton;
--- src/javascript.js
+++ src/javascript.js
@@ -1,8 +1,10 @@
 import Color from 'color';
 import { createNoise2D } from 'simplex-noise';
 import { BiColourNoise } from "./Background/Backgrounds.js";
+import DropdownButton from "./Elements/DropdownButton.js";
+import Link from "./Elements/Link.js";
 
-const primaryColour = Color.hsl(Math.random()*360, 40+Math.random()*30, 35+Math.random()*30);
+const primaryColour = Color.hsl(Math.random()*360, 70+Math.random()*30-30, 45+Math.random()*30);
 const secondaryColour = primaryColour.darken(0.35);
 
 const noise2D = createNoise2D();
@@ -25,5 +27,30 @@ const canvas = document.getElementById("Background");
 
 BiColourNoise(canvas, GenerateNoise, secondaryColour, Color('black'), 50, 15);
 
-
 document.getElementById("Content").style.backgroundColor = primaryColour.hsl().string();
+
+
+
+customElements.define("dropdown-button", DropdownButton);
+
+const more = document.getElementById("More");
+
+more.button.addEventListener("click", function(e){
+    if(more.dropdownEnabled){
+        more.button.textContent = "☷";
+    }else{
+        more.button.textContent = "☰";
+    }
+});
+
+const moreOptions = [{text:"Settings",href:"settings.html"}];
+for(let i=0; i<moreOptions.length; i++){
+    var a = document.createElement("a");
+
+    a.textContent = moreOptions[i].text;
+    a.href = moreOptions[i].href;
+
+    more.dropdown.appendChild(a);
+}
+
+more.dropdown.style.backgroundColor = primaryColour.hsl().string();
